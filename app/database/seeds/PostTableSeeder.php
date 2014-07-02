83191389466ea571e6c930b808cd2af0d0779ccb
Shengyou Fan <shengyoufan@gmail.com>
2014-07-02 12:22:46 +0800

    update: demo truncate function in seeder

--- app/database/seeds/PostTableSeeder.php
+++ app/database/seeds/PostTableSeeder.php
@@ -4,6 +4,8 @@ class PostTableSeeder extends Seeder {
 
     public function run()
     {
+        DB::table('posts')->truncate();
+
         foreach(range(1, 10) as $value)
         {
             Post::create([
